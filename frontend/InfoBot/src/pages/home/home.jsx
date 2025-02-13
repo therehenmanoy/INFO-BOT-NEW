58341e9b838e00419a2563e3adc660f0ab8da6f0
gopika-j-012 <gopikaj2026@it.ajce.in>
2025-02-13 23:09:47 +0530

    unfinished carousal

--- frontend/InfoBot/src/pages/home/home.jsx
+++ frontend/InfoBot/src/pages/home/home.jsx
@@ -1,8 +1,30 @@
+import bgImage from "../../assets/chatbg.png";
+import chat1 from "../../assets/chat1.png";
+import chat2 from "../../assets/chat2.png";
+import chat3 from "../../assets/chat3.png";
+import { Carousel } from 'flowbite-react';
+
 const HomePage = () => {
-    return(
-        <div>
-            rehen
+    return (
+        <div className="relative w-full h-screen">
+            {/* Background Image */}
+            <div
+                className="absolute inset-0 bg-cover bg-center"
+                style={{ backgroundImage: `url(${bgImage})` }}
+            ></div>
+
+            {/* Carousel Container */}
+            <div className="relative flex justify-center items-center h-full">
+                <div className="w-[300px] h-[200px]">
+                    <Carousel>
+                        <img src={chat1} alt="Slide 1" />
+                        <img src={chat2} alt="Slide 2" />
+                        <img src={chat3} alt="Slide 3" />
+                    </Carousel>
+                </div>
+            </div>
         </div>
     );
 };
-export default HomePage
+
+export default HomePage;
